4145f57954e46b134e5ef4625d16ce06d9ab6e93
shuaijl_mac <shuaijl@seas.upenn.edu>
2022-07-16 22:39:34 -0700

    update entries in python and html

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+entries = []
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "POST":
+        entry_content =request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+    
+    entries_with_date = [
+        (
+            entry[0], 
+            entry[1], 
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
